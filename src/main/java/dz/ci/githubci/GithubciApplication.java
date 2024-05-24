94a68bf60f80a503af500a4864deed4145460ee7
dlzh <2853123+dlzh@users.noreply.github.com>
2024-05-24 16:50:24 +0930

    Init commit

---
+++ src/main/java/dz/ci/githubci/GithubciApplication.java
@@ -0,0 +1,13 @@
+package dz.ci.githubci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubciApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dz/ci/githubci/GithubciApplicationTests.java
@@ -0,0 +1,13 @@
+package dz.ci.githubci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubciApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
